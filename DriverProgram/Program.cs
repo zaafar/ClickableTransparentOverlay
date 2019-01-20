e1af8ac21ef86ec4f95eeb8c0a955f6604ecc8d1
Zaafar Ahmed <zaafar.tahir@gmail.com>
2019-01-20 14:12:00 -0500

    minor QOL change

--- DriverProgram/Program.cs
+++ DriverProgram/Program.cs
@@ -11,6 +11,7 @@
         private static int Width = 2560;
         private static int Height = 1440;
         private static int Fps = 144;
+        private static int RunFor = 10;
         static void Main(string[] args)
         {
             Console.Write("Enter Screen Width:");
@@ -22,6 +23,9 @@
             Console.Write("Enter Monitor Max FPS:");
             Fps = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("You want to run this demo for X (seconds):");
+            RunFor = Convert.ToInt32(Console.ReadLine());
+
             var EndDemo = new Thread(DistroyDemo);
             EndDemo.Start();
             StartDemo();
@@ -30,13 +34,13 @@
         public static void StartDemo()
         {
             demo = new Overlay(0, 0, Width, Height, Fps);
-            demo.SubmitUI += (object sender, System.EventArgs e) => ImGui.ShowDemoWindow();
+            demo.SubmitUI += (object sender, EventArgs e) => ImGui.ShowDemoWindow();
             demo.Run();
         }
 
         public static void DistroyDemo()
         {
-            Thread.Sleep(10000);
+            Thread.Sleep(RunFor * 1000);
             //demo.ResizeWindow(0, 0, 2560, 1440);
             //Thread.Sleep(10000);
             //demo.HideWindow();
