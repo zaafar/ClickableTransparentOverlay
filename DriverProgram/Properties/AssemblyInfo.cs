e1c4b9cbba8f412916c7c255e3c13936b727f4d5
Zaafar Ahmed <zaafar.tahir@gmail.com>
2019-01-20 13:26:27 -0500

    removing unused libs

--- DriverProgram/Properties/AssemblyInfo.cs
+++ DriverProgram/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DriverProgram")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ImGui Demo Window")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DriverProgram")]
