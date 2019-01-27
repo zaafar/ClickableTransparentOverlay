b4c2ed19fbe160e87de46f1c4e3e714c93c621a5
Zaafar Ahmed <zaafar.tahir@gmail.com>
2019-01-27 13:37:24 -0500

    rename demo exe

--- DriverProgram/Properties/AssemblyInfo.cs
+++ DriverProgram/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImGuiDemo")]
-[assembly: AssemblyDescription("ImGui Demo Window")]
+[assembly: AssemblyTitle("OverlayDemo")]
+[assembly: AssemblyDescription("Overlay Demo with ImGui")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DriverProgram")]
