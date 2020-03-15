5f2474b1eb769b8410354066f4c6e5ee58f70f64
Zaafar Ahmed <zaafar.tahir@gmail.com>
2020-03-15 11:49:55 -0400

    adding full screen drawing example

--- DriverProgram/Program.cs
+++ DriverProgram/Program.cs
@@ -2,6 +2,7 @@
 {
     using ClickableTransparentOverlay;
     using ImGuiNET;
+    using System;
     using System.IO;
     using System.Numerics;
     using System.Threading;
@@ -10,17 +11,29 @@
     {
         private static bool isRunning = true;
         private static bool showImGuiDemo = false;
+
+        private static bool drawOnScreen = false;
+        private static Random randomGen = new Random();
+        private static int totalCircles = 10;
+        private static Vector2[] circleCenters = new Vector2[totalCircles];
+
         private static int Fps = 144;
         private static int[] resizeHelper = new int[4] { 0, 0, 2560, 1440 };
         private static int seconds = 5;
-        private static Overlay overlay = new Overlay(0, 0, 2560, 1440, Fps, false);
+        private static Overlay overlay = new Overlay(0, 0, 2560, 1440, Fps, true);
 
         private static void MainApplicationLogic()
         {
             while (isRunning)
             {
-                Thread.Sleep(10);
+                for (int i = 0; i < circleCenters.Length; i++)
+                {
+                    circleCenters[i].X = randomGen.Next(0, 2560);
+                    circleCenters[i].Y = randomGen.Next(0, 1440);
+                }
+                Thread.Sleep(600);
             }
+
             overlay.Dispose();
         }
 
@@ -67,6 +80,12 @@
                 }
 
                 ImGui.NewLine();
+                if(ImGui.Button("Draw on Screen"))
+                {
+                    drawOnScreen = true;
+                }
+
+                ImGui.NewLine();
                 if (File.Exists("image.png"))
                 {
                     ImGui.Image(overlay.AddOrGetImagePointer("image.png"), new Vector2(256, 256));
@@ -82,6 +101,22 @@
             if (showImGuiDemo)
             {
                 ImGui.ShowDemoWindow(ref showImGuiDemo);
+            }
+
+            if(drawOnScreen)
+            {
+                ImGui.SetNextWindowContentSize(ImGui.GetIO().DisplaySize);
+                ImGui.SetNextWindowPos(new Vector2(0, 0));
+                ImGui.Begin("Background Screen", ref drawOnScreen, ImGuiWindowFlags.NoInputs |
+                    ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoBringToFrontOnFocus |
+                    ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoScrollbar |
+                    ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoTitleBar);
+                var windowPtr = ImGui.GetWindowDrawList();
+                for (int i = 0; i < circleCenters.Length; i++)
+                {
+                    windowPtr.AddCircleFilled(circleCenters[i], 10.0f, (uint)(((255 << 24) | (00 << 16) | (00 << 8) | 255) & 0xffffffffL));
+                }
+                ImGui.End();
             }
         }
     }
