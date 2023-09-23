6745e7bedc9db95eb329e6c79a54337a3dfebeb9
Zaafar.A <zaafar.tahir@gmail.com>
2023-09-23 16:55:57 -0400

    added support for multiple fonts as well as merging fonts (#51)
    * added support for multiple fonts as well as merging fonts
    
    * added example code

--- Examples/SingleThreadedOverlayWithCoroutines/SampleOverlay.cs
+++ Examples/SingleThreadedOverlayWithCoroutines/SampleOverlay.cs
@@ -9,13 +9,16 @@
     using System.Threading.Tasks;
     using System.Numerics;
     using System;
+    using System.Drawing;
+    using System.IO;
 
     /// <summary>
     /// Render Loop and Logic Loop are synchronized.
     /// </summary>
     internal class SampleOverlay : Overlay
     {
-        private readonly ushort[] custom = new ushort[3] { 0x0020, 0xFFFF, 0x00 };
+        private ImFontPtr? font2 = null;
+        private readonly ushort[] custom1 = new ushort[3] { 0x0020, 0xFFFF, 0x00 };
         private int fontSize = 13;
         private int data;
         private string data2;
@@ -76,7 +79,7 @@
             }
         }
 
-        protected override void Render()
+        protected unsafe override void Render()
         {
             CoroutineHandler.Tick(ImGui.GetIO().DeltaTime);
             if (data % 5 == 1)
@@ -115,7 +118,61 @@
 
             if (ImGui.Button("Change Font (更改字体) Custom Range"))
             {
-                ReplaceFont(@"C:\Windows\Fonts\msyh.ttc", fontSize, custom);
+                ReplaceFont(@"C:\Windows\Fonts\msyh.ttc", fontSize, custom1);
+            }
+
+            if (ImGui.Button("Add default font"))
+            {
+                this.ReplaceFont();
+            }
+
+            if (font2 != null)
+            {
+                ImGui.PushFont(font2.Value);
+            }
+
+            if (ImGui.Button("Add two fonts (更改字体)"))
+            {
+                this.ReplaceFont(config =>
+                {
+                    var io = ImGui.GetIO();
+                    if (File.Exists(@"C:\Windows\Fonts\arial.ttf"))
+                    {
+                        io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\arial.ttf", fontSize, config, io.Fonts.GetGlyphRangesDefault());
+                    }
+
+                    fixed (ushort* p = &custom1[0])
+                    {
+                        if (File.Exists(@"C:\Windows\Fonts\msyh.ttc"))
+                        {
+                            font2 = io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\msyh.ttc", fontSize * 2, config, new IntPtr(p));
+                        }
+                    }
+                });
+            }
+
+            if (font2 != null)
+            {
+                ImGui.PopFont();
+            }
+
+            if (ImGui.Button("Merge two Fonts (\uf2b8 + \uf592 + 更改字体)"))
+            {
+                this.ReplaceFont(config =>
+                {
+                    var io = ImGui.GetIO();
+                    io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\msyh.ttc", fontSize, config, io.Fonts.GetGlyphRangesChineseSimplifiedCommon());
+                    config->MergeMode = 1;
+                    config->OversampleH = 1;
+                    config->OversampleV = 1;
+                    config->PixelSnapH = 1;
+
+                    var custom2 = new ushort[] { 0xe005, 0xf8ff, 0x00 };
+                    fixed (ushort* p = &custom2[0])
+                    {
+                        io.Fonts.AddFontFromFileTTF("fa-brands-400.ttf", fontSize, config, new IntPtr(p));
+                    }
+                });
             }
 
             if (ImGui.Button("Show/Hide Demo Window"))
